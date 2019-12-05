08189f6c02825aaaaf735ebc06fa612dd97295cb
Michael Mitteregger <michael.mitteregger@bearingpoint.com>
2019-12-05 13:36:55 +0100

    removes unused function `getStartpageMenusByThemeId` in Menu.php

--- application/models/Menu.php
+++ application/models/Menu.php
@@ -76,20 +76,4 @@ class Dao_Menu extends Dao_Abstract
         return $table->fetchAll($select);
     }
 
-    public function getStartpageMenusByThemeId($themeId)
-    {
-        $table = new Db_Menu();
-
-        $select = $table->select(Zend_Db_Table::SELECT_WITH_FROM_PART);
-        $select->setIntegrityCheck(false)
-            ->join(Db_Theme::TABLE_NAME,
-                Db_Theme::TABLE_NAME . '.' . Db_Theme::MENU_ID . ' = ' . Db_Menu::TABLE_NAME . '.' . Db_Menu::ID)
-            ->where(Db_Theme::TABLE_NAME . '.' . Db_Theme::ID . ' = ?', $themeId)
-            ->order(Db_Menu::ORDER_NUMBER);
-
-        $rows = $table->fetchRow($select);
-
-        return $rows;
-    }
-
 }
