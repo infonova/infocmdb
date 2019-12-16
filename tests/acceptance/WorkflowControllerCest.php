3f228b06abb48b44fe384c8e98eea32a9a445ac2
Michael Mitteregger <michael.mitteregger@bearingpoint.com>
2019-12-16 10:04:58 +0100

    fixes displaying workflow json context

--- tests/acceptance/WorkflowControllerCest.php
+++ tests/acceptance/WorkflowControllerCest.php
@@ -141,4 +141,115 @@ class WorkflowControllerCest extends AbstractAcceptanceTest
         $I->seeInSource('title="Fehler gelöst"');
         $I->dontSeeInSource('title="Als gelöst markieren"');
     }
+
+    public function testWorkflowInstanceJsonContext(AcceptanceTester $I)
+    {
+        $jsonTestData = <<< JSON
+        {
+            "Environment": {
+            "APPLICATION_ENV": "production",
+    "APPLICATION_PATH": "\/mnt\/webroot\/sdcmdb\/application",
+    "APPLICATION_URL": "http:\/\/sdcmdbdev.infonova.at\/",
+    "APPLICATION_DATA": "\/mnt\/webroot\/sdcmdb\/data",
+    "APPLICATION_PUBLIC": "\/mnt\/webroot\/sdcmdb\/public\/",
+    "GOCACHE": "\/mnt\/webroot\/sdcmdb\/data\/cache\/golang",
+    "WORKFLOW_CONFIG_PATH": "\/mnt\/webroot\/sdcmdb\/application\/configs\/workflows"
+  },
+  "ciid": 184113,
+  "triggerType": "ci_update",
+  "data": {
+            "old": {
+                "relations": {
+                },
+      "projects": {
+                    "23": {
+                        "id": "23",
+          "name": "emea",
+          "description": "BE-EMEA",
+          "note": "Bearingpoint EMEA",
+          "order_number": "0",
+          "is_active": "1",
+          "user_id": "0",
+          "valid_from": "0000-00-00 00:00:00",
+          "ci_project_valid_from": "2011-07-01 10:16:58",
+          "ci_project_history_id": "818162"
+        }
+      },
+      "ciTypeId": "275",
+      "ciTypeName": "user_austria_graz",
+      "attributes": {
+                    "301": {
+                        "2608582": {
+                            "id": "301",
+            "name": "ma_sap_no",
+            "description": "http://username:password@webservice.call.com",
+            "note": "SAT file column A 'SAP-No'",
+            "hint": "SAT file column A \"SAP-No\"",
+            "attribute_type_id": "1",
+            "attribute_group_id": "1",
+            "order_number": "50",
+            "column": "1",
+            "is_unique": "1",
+            "is_numeric": "0",
+            "is_bold": "0",
+            "is_event": "0",
+            "is_unique_check": "0",
+            "is_autocomplete": "0",
+            "is_multiselect": "0",
+            "is_project_restricted": "0",
+            "regex": "",
+            "workflow_id": null,
+            "script_name": null,
+            "tag": null,
+            "input_maxlength": "15",
+            "textarea_cols": "0",
+            "textarea_rows": "0",
+            "is_active": "1",
+            "user_id": "0",
+            "valid_from": "2012-01-02 08:33:47",
+            "historicize": "1",
+            "display_style": null,
+            "attributeTypeName": "input",
+            "attribute_group": "General",
+            "parent_attribute_group": "0",
+            "value_text": "4041531",
+            "value_date": null,
+            "value_ci": null,
+            "ciAttributeId": "2608582",
+            "initial": "0",
+            "valueNote": null,
+            "history_id": "928883",
+            "value_default": null
+          }
+        }
+      }
+    }
+  },
+  "user_id": "126"
+}
+JSON;
+        $I->wantTo('See valid json context');
+
+        $instanceId = $I->haveInDatabase('workflow_case', [
+            'workflow_id' => 1,
+            'context' => $jsonTestData,
+            'status' => 'CLOSED',
+            'user_id' => 0,
+        ]);
+
+        $I->amOnPage('/workflow/instance/instanceId/' . $instanceId);
+
+        $I->cantSeeJsError();
+
+        $contextContainer = $I->findElement(WebDriverBy::id("context_container"));
+        $contextContainerHtml = $contextContainer->getAttribute('innerHTML');
+        $I->assertNotEmpty($contextContainerHtml, "context should be displayed");
+
+        $I->wantTo('See no credentials in json context');
+        $I->assertNotContains("http://username:password@webservice.call.com", $contextContainerHtml,
+            "credentials should be hidden");
+        $I->assertContains("http://<credentials>@webservice.call.com", $contextContainerHtml,
+            "credentials should be hidden");
+    }
+
 }
