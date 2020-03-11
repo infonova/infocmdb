474e6803a84f4bea06e166cc917a1d6a3a393848
Michael Mitteregger <michael.mitteregger@bearingpoint.com>
2020-03-11 11:28:20 +0100

    Update triggers on attribute_default_queries to include new column list_query

--- deploy/migrations/20200309095933_add_list_query.php
+++ deploy/migrations/20200309095933_add_list_query.php
@@ -21,11 +21,57 @@ class AddListQuery extends AbstractPhinxMigration
                 from " . $this->dbName . "_tables.attribute_default_queries
                 WITH CASCADED CHECK OPTION
         ");
+
+        $this->execute("
+                CREATE OR REPLACE DEFINER = 'root'@localhost 
+                    TRIGGER `" . $this->dbName . "_tables`.attribute_default_queries_update 
+                    BEFORE UPDATE 
+                    ON attribute_default_queries
+                    FOR EACH ROW 
+                BEGIN
+                    INSERT INTO " . $this->dbName . "_history.attribute_default_queries (id, attribute_id, query, list_query, valid_from, valid_to)
+                    VALUES (OLD.id, OLD.attribute_id, OLD.query, OLD.list_query, OLD.valid_from, NOW());
+                END;
+        ");
+        $this->execute("
+                CREATE OR REPLACE DEFINER = 'root'@localhost 
+                    TRIGGER `" . $this->dbName . "_tables`.attribute_default_queries_delete 
+                    BEFORE DELETE 
+                    ON attribute_default_queries
+                    FOR EACH ROW 
+                BEGIN
+                    INSERT INTO " . $this->dbName . "_history.attribute_default_queries (id, attribute_id, query, list_query, valid_from, valid_to)
+                    VALUES (OLD.id, OLD.attribute_id, OLD.query, OLD.list_query, OLD.valid_from, NOW());
+                END;
+        ");
     }
 
     public function down()
     {
         $table = $this->table('attribute_default_queries');
+
+        $this->execute("
+                CREATE OR REPLACE DEFINER = 'root'@localhost 
+                    TRIGGER `" . $this->dbName . "_tables`.attribute_default_queries_update 
+                    BEFORE UPDATE 
+                    ON attribute_default_queries
+                    FOR EACH ROW 
+                BEGIN
+                    INSERT INTO " . $this->dbName . "_history.attribute_default_queries (id, attribute_id, query, valid_from, valid_to)
+                    VALUES (OLD.id, OLD.attribute_id, OLD.query, OLD.valid_from, NOW());
+                END;
+        ");
+        $this->execute("
+                CREATE OR REPLACE DEFINER = 'root'@localhost 
+                    TRIGGER `" . $this->dbName . "_tables`.attribute_default_queries_delete 
+                    BEFORE DELETE 
+                    ON attribute_default_queries
+                    FOR EACH ROW 
+                BEGIN
+                    INSERT INTO " . $this->dbName . "_history.attribute_default_queries (id, attribute_id, query, valid_from, valid_to)
+                    VALUES (OLD.id, OLD.attribute_id, OLD.query, OLD.valid_from, NOW());
+                END;
+        ");
 
         if ($table->hasColumn('list_query')) {
             $this->execute("ALTER TABLE `" . $this->dbName . "_tables`.`attribute_default_queries` DROP COLUMN `list_query`");
